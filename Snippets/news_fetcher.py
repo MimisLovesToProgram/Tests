19e1fde3ff4195a29fde1e0965daa83cecce52a1
MimisLovesToProgram <dimitris_the_programmer@yahoo.com>
2024-01-16 14:49:38 +0200

    Wrote the solution to be submitted for phase A's problem, and added the new news_fetcher script-snippet.

---
+++ Snippets/news_fetcher.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+# Setting up URL with API key and company names
+company_names = ['Microsoft', 'Apple', 'Amazon', 'Facebook', 'Google']
+url = ('https://newsapi.org/v2/everything?'
+       'q=' + ' OR '.join(company_names) + '&'
+       'from=' + 'TWO DAYS FROM TODAY, CHANGE THIS IN PROD' + '&' # Emphasized Item Here
+       'to=' + 'TODAY, CHANGE THIS IN PROD AS WELL' + '&' # Here As Well
+       'sortBy=publishedAt&'
+       'apiKey=YOUR API KEY, DONT FORGET TO CHANGE THIS IN PROD. GET THE KEY WHEN SIGNING UP') # Another One Here.
+
+# Trying to send request to get news
+try:
+    response = requests.get(url)
+except:
+    print("Can't access link, please check your internet")
+    # exit()
+
+news = json.loads(response.text)
+
+# Loop to read new articles, handle the data however you want, by using the tags 'title' or 'description', advisably.
+for new in news['articles']:
+    print("##############################################################\n")
+    print(str(new['title']), "\n\n")
+    print('______________________________________________________\n')
+    print(str(new['description']), "\n\n") # Also useful when evaluating the headline's positivity. This is basically a brief overview of the article.
+    print("..............................................................")
