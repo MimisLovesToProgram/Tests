f17aac3b81d9e2f0f583fd2efd662958b812e03e
MimisLovesToProgram <dimitris_the_programmer@yahoo.com>
2024-01-17 22:27:58 +0200

    Deleted the .vscode folder, and added a simple way to look for preset safe company's tickers in the headline_analyzer.py script.

--- Snippets/headline_analyzer.py
+++ Snippets/headline_analyzer.py
@@ -1,11 +1,21 @@
-# After 'pip install nltk', you may need to run these commands on the python shell for the package to be ready to use:
+# After 'pip install nltk', you will need to run these commands on the python shell for the package to be ready to use:
 # import nltk
 # nltk.download('vader_lexicon')
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+# Defining a dictionary of 'safe' companies and their Ticker Symbols:
+safe_company_tickers = {"Berkshire Hathaway": "BRK.A",
+                        "The Walt Disney Company": "DIS",
+                        "Vanguard High-Dividend Yield ETF": "VYM",
+                        "Procter & Gamble": "PG",
+                        "Vanguard Real Estate Index Fund": "VNQ",
+                        "Starbucks": "SBUX",
+                        "Apple": "AAPL",
+                        "Quest Diagnostics": "DGX",}
+
 # Define the input text
-input_text = "Apples Are Good For You - Here's Why."
+input_text = "Apple Announces Record-Braking Profits for 2024."
 
 # Initialize the sentiment analyzer
 sia = SentimentIntensityAnalyzer()
@@ -17,3 +27,10 @@ sentiment_scores = sia.polarity_scores(input_text)
 
 # Print the sentiment scores
 print(sentiment_scores)
+
+company = ""
+for word in input_text.split():
+    if word in safe_company_tickers:
+        company = word
+
+print(f"Company's Ticker: {safe_company_tickers[company]}")
