7dc319baf56e884e7a131897bd1be36d9e795190
MimisLovesToProgram <dimitris_the_programmer@yahoo.com>
2024-01-28 15:42:22 +0200

    Deleted unecessary lines in headline_analyzer.py.

--- Snippets/headline_analyzer.py
+++ Snippets/headline_analyzer.py
@@ -4,19 +4,8 @@
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-# Defining a dictionary of 'safe' companies and their Ticker Symbols:
-safe_company_tickers = {"Berkshire Hathaway": "BRK.B",
-                        "The Walt Disney Company": "DIS",
-                        "Vanguard High-Dividend Yield ETF": "VYM",
-                        "Procter & Gamble": "PG",
-                        "Vanguard Real Estate Index Fund": "VNQ",
-                        "Starbucks": "SBUX",
-                        "Apple": "AAPL",
-                        "Quest Diagnostics": "DGX",
-                        "Microsoft": "MSFT"}
-
 # Define the input text
-input_text = "Apple Announces Record-Braking Profits for 2024."
+input_text = "Hello."
 
 # Initialize the sentiment analyzer
 sia = SentimentIntensityAnalyzer()
@@ -28,10 +17,3 @@ sentiment_scores = sia.polarity_scores(input_text)
 
 # Print the sentiment scores
 print(sentiment_scores)
-
-company = ""
-for word in input_text.split():
-    if word in safe_company_tickers:
-        company = word
-
-print(f"Company's Ticker: {safe_company_tickers[company]}")
